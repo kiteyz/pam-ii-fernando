eb4a1780cfd631aef00b804bd88852636b150d5f
Fernando Thomaz <fernando.thomaz27@gmail.com>
2024-03-20 08:31:21 -0300

    Criando primeiro arquivo JavaScript

---
+++ 01/methods-and-variables.js
@@ -0,0 +1,9 @@
+let variableOne = "olá";
+var variableTwo = 3;
+const variableThree = 8;
+
+console.log("olá mundo!");
+
+console.log(variableOne, "mundo!");
+
+console.log(variableTwo + variableThree);
